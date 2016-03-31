472b3a6b61a2df67bf9f207a2311ee5260477497
k <k@debian>
2016-03-31 22:46:39 +1030

    Fixed cylinder.cpp and it seems to run fine.

--- 2016CP1Week4/cylinder.cpp
+++ 2016CP1Week4/cylinder.cpp
@@ -19,8 +19,8 @@
 using namespace std;
 double PI = 3.14159; // This variable is defined globally, known to all
 // functions in this program as PI
-double cross area(double); // Function declaration for function cross area
-double side area(double, double); // Function declaration for function side area
+double cross_area(double); // Function declaration for function cross area
+double side_area(double, double); // Function declaration for function side area
 
 int main() {
     double h, r; //variables local to the main function
@@ -31,18 +31,18 @@ int main() {
     cout << "you have entered r = " << r << " and h = " << h << "." << endl;
     cout << "I am planning to use inch, thus in the first function, I will convert r, and " << endl;
     cout << "in the second one I will convert h \n";
-    cout << "The cross section area of the cylinder is " << cross area(r) << " inch-sqr" << endl;
-    cout << "The side area of the cylinder is " << side area(r, h) << " inch-sqr \n\n";
+    cout << "The cross section area of the cylinder is " << cross_area(r) << " inch-sqr" << endl;
+    cout << "The side area of the cylinder is " << side_area(r, h) << " inch-sqr \n\n";
     return 0;
 }
 
-double cross area(double r) {
+double cross_area(double r) {
     //Cross section area includes the disks at the bottom and the top
     r = r * 0.3937; // converting r to inch
     return 2 * PI * pow(r, 2);
 }
 
-double side area(double r, double h) {
+double side_area(double r, double h) {
     double area; //variable local to side area function
     h = h * 0.3937; // converting h to inch
     area = 2 * PI * r*h;
